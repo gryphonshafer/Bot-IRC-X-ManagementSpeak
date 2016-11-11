38f7fcfc77a080e64bce4ddb8129ff2b711d050e
Gryphon Shafer <gryphon@goldenguru.com>
2016-11-10 18:03:04 -0800

    Initial commit for v1.01

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Bot-IRC-X-ManagementSpeak
-Bot::IRC plugin for rendering management-speak
+# NAME
+
+Bot::IRC::X::ManagementSpeak - Bot::IRC plugin for rendering management-speak
+
+# VERSION
+
+version 1.01
+
+[![Build Status](https://travis-ci.org/gryphonshafer/Bot-IRC-X-ManagementSpeak.svg)](https://travis-ci.org/gryphonshafer/Bot-IRC-X-ManagementSpeak)
+[![Coverage Status](https://coveralls.io/repos/gryphonshafer/Bot-IRC-X-ManagementSpeak/badge.png)](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-ManagementSpeak)
+
+# SYNOPSIS
+
+    use Bot::IRC;
+
+    Bot::IRC->new(
+        connect => { server => 'irc.perl.org' },
+        plugins => ['ManagementSpeak'],
+    )->run;
+
+# DESCRIPTION
+
+This [Bot::IRC](https://metacpan.org/pod/Bot::IRC) plugin is for rendering management-speak.
+
+    <bot> mspeak
+
+# SEE ALSO
+
+You can look for additional information at:
+
+- [Bot::IRC](https://metacpan.org/pod/Bot::IRC)
+- [GitHub](https://github.com/gryphonshafer/Bot-IRC-X-ManagementSpeak)
+- [CPAN](http://search.cpan.org/dist/Bot-IRC-X-ManagementSpeak)
+- [MetaCPAN](https://metacpan.org/pod/Bot::IRC::X::ManagementSpeak)
+- [AnnoCPAN](http://annocpan.org/dist/Bot-IRC-X-ManagementSpeak)
+- [Travis CI](https://travis-ci.org/gryphonshafer/Bot-IRC-X-ManagementSpeak)
+- [Coveralls](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-ManagementSpeak)
+- [CPANTS](http://cpants.cpanauthors.org/dist/Bot-IRC-X-ManagementSpeak)
+- [CPAN Testers](http://www.cpantesters.org/distro/T/Bot-IRC-X-ManagementSpeak.html)
+
+# AUTHOR
+
+Gryphon Shafer <gryphon@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Gryphon Shafer.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
---
+++ lib/Bot/IRC/X/ManagementSpeak.pm
@@ -0,0 +1,76 @@
+package Bot::IRC::X::ManagementSpeak;
+# ABSTRACT: Bot::IRC plugin for rendering management-speak
+
+use strict;
+use warnings;
+
+use Lingua::ManagementSpeak;
+
+# VERSION
+
+sub init {
+    my ($bot) = @_;
+
+    my $ms = Lingua::ManagementSpeak->new;
+
+    $bot->hook(
+        {
+            to_me => 1,
+            text  => qr/^mspeak/i,
+        },
+        sub {
+            my ( $bot, $in, $m ) = @_;
+            $bot->reply( $ms->paragraph(4) );
+        },
+    );
+
+    $bot->helps( mspeak => 'Return managerial-sounding text. Usage: <bot> mspeak.' );
+}
+
+1;
+__END__
+=pod
+
+=begin :badges
+
+=for markdown
+[![Build Status](https://travis-ci.org/gryphonshafer/Bot-IRC-X-ManagementSpeak.svg)](https://travis-ci.org/gryphonshafer/Bot-IRC-X-ManagementSpeak)
+[![Coverage Status](https://coveralls.io/repos/gryphonshafer/Bot-IRC-X-ManagementSpeak/badge.png)](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-ManagementSpeak)
+
+=end :badges
+
+=for test_synopsis BEGIN { die "SKIP: Bot::IRC may or may not be available." }
+
+=head1 SYNOPSIS
+
+    use Bot::IRC;
+
+    Bot::IRC->new(
+        connect => { server => 'irc.perl.org' },
+        plugins => ['ManagementSpeak'],
+    )->run;
+
+=head1 DESCRIPTION
+
+This L<Bot::IRC> plugin is for rendering management-speak.
+
+    <bot> mspeak
+
+=head1 SEE ALSO
+
+You can look for additional information at:
+
+=for :list
+* L<Bot::IRC>
+* L<GitHub|https://github.com/gryphonshafer/Bot-IRC-X-ManagementSpeak>
+* L<CPAN|http://search.cpan.org/dist/Bot-IRC-X-ManagementSpeak>
+* L<MetaCPAN|https://metacpan.org/pod/Bot::IRC::X::ManagementSpeak>
+* L<AnnoCPAN|http://annocpan.org/dist/Bot-IRC-X-ManagementSpeak>
+* L<Travis CI|https://travis-ci.org/gryphonshafer/Bot-IRC-X-ManagementSpeak>
+* L<Coveralls|https://coveralls.io/r/gryphonshafer/Bot-IRC-X-ManagementSpeak>
+* L<CPANTS|http://cpants.cpanauthors.org/dist/Bot-IRC-X-ManagementSpeak>
+* L<CPAN Testers|http://www.cpantesters.org/distro/T/Bot-IRC-X-ManagementSpeak.html>
+
+=for Pod::Coverage init
+
+=cut
